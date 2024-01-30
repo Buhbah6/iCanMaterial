59d37806207a4aca5c44adb136889c93c4187195
Tony Nadeau <thegoldenrailway@gmail.com>
2024-01-30 13:38:00 -0500

    Updated Change Page

--- class_documents/code/ChangePage.py
+++ class_documents/code/ChangePage.py
@@ -18,6 +18,20 @@ def changePage(root, page1Frame, page2Frame):
         page2Frame.pack_forget()
         page1Frame.pack(fill='both', expand=True)
 
+def onEnter(button):
+    global page
+    if page == 1:
+        button.config(background='light blue')
+    elif page == 2:
+        button.config(background='red')
+
+def onLeave(button):
+    global page
+    if page == 1:
+        button.config(background='red')
+    elif page == 2:
+        button.config(background='light blue')
+
 # UI code
 root = tk.Tk()
 root.geometry("800x500")
@@ -38,5 +52,11 @@ text2 = tk.Label(page2Frame, text=p2Text, bg='red', font=('Arial', 22, 'bold'))
 text2.pack(fill='both', expand=True)
 button2 = tk.Button(page2Frame, text="Change Page", command=lambda: changePage(root, page1Frame, page2Frame), bg='light blue', font=('Arial', 18))
 button2.pack(padx=20, pady=20, fill='both', expand=True)
+
+button1.bind("<Enter>", lambda event: onEnter(button1))
+button1.bind("<Leave>", lambda event: onLeave(button1))
+
+button2.bind("<Enter>", lambda event: onEnter(button2))
+button2.bind("<Leave>", lambda event: onLeave(button2))
 
 root.mainloop()
